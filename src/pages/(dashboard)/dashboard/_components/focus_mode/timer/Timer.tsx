3bbadc536a8d902d0c10092a424126695d7344da
Joshuamjv2 <joshuamjv22@gmail.com>
2024-08-25 19:16:11 +0300

    favicon

--- src/pages/(dashboard)/dashboard/_components/focus_mode/timer/Timer.tsx
+++ src/pages/(dashboard)/dashboard/_components/focus_mode/timer/Timer.tsx
@@ -10,14 +10,15 @@ export default function Timer(){
     const [pause, setPause] = useState(false)
     return (
         <div className="flex flex-col gap-8 items-center mt-8">
-            <div className="bg-white bg-opacity-35 w-56 h-56 rounded-full p-2 flex items-center justify-center">
-                <div className="w-48 h-48">
-                    <CircularProgressbar value={50} text="50%" strokeWidth={6}
+            <div className="bg-white bg-opacity-40 w-60 h-60 rounded-full p-2 flex items-center justify-center">
+                <div className="w-52 h-52">
+                    <CircularProgressbar value={50} text="25:00:00" strokeWidth={4}
                         styles={buildStyles({
                             rotation: 1, strokeLinecap: "round",
-                            textColor: "#fff",
+                            textColor: "#071a37",
                             pathColor: "#29a259",
-                            trailColor: "rgba(255, 255, 255, .8)"
+                            trailColor: "rgba(255, 255, 255, .8)",
+                            textSize: "12px"
                         })}
                     />
                 </div>
