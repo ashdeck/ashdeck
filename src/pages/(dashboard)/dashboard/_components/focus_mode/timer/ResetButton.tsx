154e5339525ff58e83ba21aaaa982f9c05e14ea6
Joshuamjv2 <joshuamjv22@gmail.com>
2024-11-18 21:22:25 +0300

    add icons

--- src/pages/(dashboard)/dashboard/_components/focus_mode/timer/ResetButton.tsx
+++ src/pages/(dashboard)/dashboard/_components/focus_mode/timer/ResetButton.tsx
@@ -1,7 +1,8 @@
+import { Refresh } from "iconsax-react"
 export default function ResetButton({onClick}){
     return(
         <div onClick={onClick}>
-            <button className="bg-slate-400 text-white font-semibold rounded-md outline-none p-2 w-36">Reset</button>
+            <button className="bg-slate-400 text-white font-semibold rounded-md outline-none p-2"><Refresh /></button>
         </div>
     )
 }
