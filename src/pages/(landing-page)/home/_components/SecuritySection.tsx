2faced696c509cc3b01020548aa23d432f9227d3
Joshuamjv2 <joshuamjv22@gmail.com>
2025-02-09 19:01:54 +0300

    add emojis and security lock

--- src/pages/(landing-page)/home/_components/SecuritySection.tsx
+++ src/pages/(landing-page)/home/_components/SecuritySection.tsx
@@ -3,6 +3,8 @@ import HeadingText from "@components/HeadingText"
 import Link from "@router/link"
 import { ArrowUpRightIcon } from "@heroicons/react/24/outline"
 import AddToChrome from "@/src/commons/components/AddToChrome"
+import LockAnimation from "../../../../../src/commons/assets/anim/lock-animation.json";
+import Lottie from "react-lottie-player";
 
 
 type Props = {
@@ -14,10 +16,19 @@ const SecuritySection = ({ className }: Props) => {
 		<div
 			className={"flex flex-col h-fit w-full items-center gap-4 md:gap-6 lg:gap-12 mb-36 px-[5%] md:px-[15%]"}>
 
-			<HeadingText className="text-center mt-[10%] max-w-[90%]  md:max-w-[40rem] text-2xl md:3xl lg:text-5xl drop-shadow-2xl shadow-primary">
-				Our Dedication to Your Privacy
-			</HeadingText>
+			<div className=" flex items-center justify-center flex-col">
+				<Lottie
+				className="h-20 w-20 md:h-40 md:w-40"
+				play
+				segments={[45, 150]}
+				animationData={LockAnimation}
+				>
+				</Lottie>
 
+				<HeadingText className="text-centermax-w-[90%]  md:max-w-[40rem] text-2xl md:3xl lg:text-5xl drop-shadow-2xl shadow-primary">
+					Our Dedication to Your Privacy
+				</HeadingText>
+				</div>
 
 			<p className={"max-w-[90%] md:max-w-[80%] md:text-md lg:text-lg text-gray-600 text-center"}>
 				At Ashdeck, protecting your privacy is our priority. We don’t monitor, track, or share your
