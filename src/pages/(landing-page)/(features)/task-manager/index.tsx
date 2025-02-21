8a74627ba61d850b606c45d613052c2efe0902e5
Joshuamjv2 <joshuamjv22@gmail.com>
2025-02-21 09:40:11 +0300

    add for who section

--- src/pages/(landing-page)/(features)/task-manager/index.tsx
+++ src/pages/(landing-page)/(features)/task-manager/index.tsx
@@ -3,6 +3,7 @@ import FAQSection from "../../home/_components/FAQ"
 import SecuritySection from "../../home/_components/SecuritySection"
 import HeroSection from "../../home/_components/Hero.section";
 import HowItWorksSection from "../../home/_components/HowItWorks.section";
+import ForWhoSection from "../../home/_components/ForWho.section";
 
 type FAQItem = {
     question: string;
@@ -75,10 +76,60 @@ function TaskManager({}) {
         title: "Stay Organized and Get More Done with Ashdeck’s Task Manager",
         details: "Ashdeck’s Task Manager helps you structure your day, prioritize tasks, and track your progress—all in one distraction-free workspace. Whether you’re tackling a major project, organizing daily tasks, or simply trying to stay on top of your workload, Ashdeck helps you work smarter."
     }
+
+    const for_who = [
+      {
+        title: "Students",
+        description: "Accelerate your academic journey and achieve top grades while saving time and money.",
+        image: <img className={"w-[30%] absolute"} src={"/images/student.png"} alt={""} />,
+      },
+      {
+        title: "Developers",
+        description: "Dive deep into your work without interruptions and deliver your best code yet.",
+        image: <img className={"w-[35%] absolute"} src={"/images/pc.png"} alt={""} />,
+      },
+      {
+        title: "Marketers",
+        description: "Cut through the noise with focus tools that let you block distractions. Stay ahead of the competition by using a website blocker to zero in on what drives results.",
+        image: <img className={"w-[25%] absolute"} src={"/images/megaphone.png"} alt={""} />,
+      },
+      {
+        title: "Writers",
+        description: "Unleash your creativity uninterrupted. A free website blocker helps you stay in your flow, so you can craft your best work with no distractions.",
+        image: <img className={"w-[40%] absolute"} src={"/images/papers.png"} alt={""} />,
+      },
+      {
+        title: "Entrepreneurs",
+        description: "Strategize smarter. Use focus techniques like the Pomodoro timer to make every minute count, and watch your business thrive like never before.",
+        image: <img className={"w-[25%] absolute"} src={"/images/coin.png"} alt={""} />,
+      },
+      {
+        title: "Freelancers",
+        description: "Take control of your workload. With tools like a website blocker, balance productivity and freedom—your time is yours to own.",
+        image: <img className={"w-[20%] absolute"} src={"/images/freelancer.png"} alt={""} />,
+      },
+      {
+        title: "Designers",
+        description: "Bring your ideas to life in a space free of interruptions. Whether it’s using a Pomodoro timer or blocking distractions, create without limits.",
+        image: <img className={"w-[25%] absolute"} src={"/images/palette.png"} alt={""} />,
+      },
+      {
+        title: "Researchers",
+        description: "Focus deeply, discover boldly. A free website blocker can help you stay in the zone and achieve the breakthroughs you aim for.",
+        image: <img className={"w-[25%] absolute"} src={"/images/researcher.png"} alt={""} />,
+      },
+      {
+        title: "For those with ADHD",
+        description: "Your mind is brilliant. A little structure—like a website blocker or Pomodoro timer—can help you channel your energy and own your day.",
+        image: <img className={"w-[25%] absolute"} src={"/images/adhd2.png"} alt={""} />,
+      }
+    ]
+
 	return (
 		<main className={"flex flex-col bg-white dark:bg-bg-dark"}>
             <HeroSection headline={hero.headline} details={hero.detail} />
             <HowItWorksSection featured_items={how_it_works.features} title={how_it_works.title} details={how_it_works.details} />
+            <ForWhoSection forWhoItems={for_who} />
 			<SecuritySection />
 			<FAQSection faqItems={faqItems} />
 			<CtaSection headline={cta.headline} detail={cta.detail} />
