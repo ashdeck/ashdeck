8bf4ba678e97366c80f48a528a7ae523c55ffd84
Joshuamjv2 <joshuamjv22@gmail.com>
2025-03-01 00:38:30 +0300

    mobile emojis

--- src/pages/(landing-page)/home/_components/ForWho.section.tsx
+++ src/pages/(landing-page)/home/_components/ForWho.section.tsx
@@ -8,6 +8,7 @@ type ForWhoItem = {
   	title: string;
   	description: string;
 	image: string
+	emoji?: string
 };
 
 type Props = {
@@ -69,7 +70,7 @@ const FeatureCard = ({ item, index = 0 }) => {
 
 			<div className="sm:w-[50%] flex flex-col px-8 sm:px-12 md:px-20 py-10">
 				<h3 className={"font-heading font-medium text-2xl"}>
-					{item?.title}
+					{item?.title} <span className="sm:hidden">{item.emoji}</span>
 				</h3>
 
 				<p className={"text-sm my-4"}>
